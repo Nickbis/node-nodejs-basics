f254c374966aa931756b8ed179a6f0402265d540
@Nickbis <nickbis3@gmail.com>
2024-01-30 00:02:34 +0300

    feat: complit FS task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { rm } from "node:fs/promises";
+import { join } from "node:path";
+import getDirname from "../utils/getDirname.js";
+
 const remove = async () => {
-    // Write your code here 
+    const currentDirectory = getDirname(import.meta.url);
+    const fileName = "fileToRemove.txt";
+    const pathToFile = "/files";
+
+    try {
+        await rm(join(currentDirectory, pathToFile, fileName));
+        console.log("The task is completed");
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        }
+        throw err;
+    }
 };
 
 await remove();
